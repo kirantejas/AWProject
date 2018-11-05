bceb874d6a80a54d9cf67f7fea00a35d0cb129ae
skodumur <skodumur@asu.edu>
2018-11-04 20:06:37 -0700

    first commit

---
+++ Assets/MoveLeft.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveLeft : MonoBehaviour {
+    Vector3 movement;
+	// Use this for initialization
+	void Start () {
+        movement = new Vector3(20, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onMLClick()
+    {
+        //Vector3 test = Vector3
+        GameObject jeep;
+        jeep = GameObject.Find("jeep");
+        jeep.transform.Translate(movement*Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    Vector3 movement;
+	// Use this for initialization
+	void Start () {
+        movement = new Vector3(0, 150, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(movement * Time.deltaTime);
+	}
+}
