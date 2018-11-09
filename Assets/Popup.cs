084647df2ef77fc8831b4695a6d972a68a303bf3
skodumur <skodumur@asu.edu>
2018-11-08 19:22:46 -0700

    Added popup

---
+++ Assets/Popup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour {
+
+    public GameObject window;
+    public Text messageField;
+
+    public void Show(string message)
+    {
+        messageField.text = message;
+        window.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        window.SetActive(false);
+    } 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
