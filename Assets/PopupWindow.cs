53a501ba43d8b4cea70a977b740328587c470255
Dharmesh Ch <dharmeshch@dharmeshs-mbp.3521.dhcp.asu.edu>
2018-11-11 15:56:36 -0700

    added popup

---
+++ Assets/PopupWindow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupWindow : MonoBehaviour
+{
+
+	public GameObject window;
+	public Text messageField;
+	float delayTime = 10f;
+	float timeElapsed;
+	int check=0;
+	// Use this for initialization
+
+	void start(){
+		
+	}
+
+	void update(){
+		if (check == 0) {
+			timeElapsed += Time.deltaTime;
+			if (timeElapsed > delayTime) {
+				check = 1;
+				messageField.text = "hello world";
+				window.SetActive (true);
+
+			}
+		}
+	}
+	public void Show(string message) {
+		messageField.text = message;
+		window.SetActive (true);
+	}
+		
+	
+	// Update is called once per frame
+	public void Hide () {
+		window.SetActive (false);
+	}
+}
