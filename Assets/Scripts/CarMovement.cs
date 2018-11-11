f3029b730b9cc3541d4b456a70e0053cce9cdeb6
Kiran Teja Settipalli <kiranteja.settipalli@gmail.com>
2018-11-11 15:54:39 -0700

    level 4

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // The target marker.
+    public Transform target;
+
+    // Speed in units per sec.
+    public float speed = 10;
+
+    void Update()
+    {
+        // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+        // Set target position
+        target.position = new Vector3(-0.344f, 0.24f, 0.309f);
+        // Move our position a step closer to the target.
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ Assets/Scripts/Level3.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3 : MonoBehaviour {
+
+    GameObject jeep;
+    GameObject wheel;
+    public static bool isRotateJeep;
+    public static bool isRotateWheel;
+
+    // Use this for initialization
+    void Start () {
+        isRotateJeep = false;
+        isRotateWheel = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isRotateJeep)
+        {
+            jeep = GameObject.Find("greenjeep");
+            jeep.transform.Rotate(new Vector3(0, Time.deltaTime * 500, 0)); 
+        }
+
+        if (isRotateWheel)
+        {
+            wheel = GameObject.Find("WheelBall");
+            wheel.transform.Rotate(new Vector3(Time.deltaTime * 500, 0, 0));
+        }
+    }
+
+    public void RotateCar()
+    {
+        isRotateJeep = true;
+    }
+    public void StopCar()
+    {
+        isRotateJeep = false;
+    }
+    public void RotateWheel()
+    {
+        isRotateWheel = true;
+    }
+    public void StopWheel()
+    {
+        isRotateWheel = false;
+    }
+}
