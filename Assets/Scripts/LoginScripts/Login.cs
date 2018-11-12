4bb0ac3348ce619c14b6dc1545029d8d6396a218
Kiran Teja Settipalli <kiranteja.settipalli@gmail.com>
2018-11-12 01:12:07 -0700

    Added Welcome screen and Login screen

---
+++ Assets/Scripts/LoginScripts/Login.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour {
+ #region Variables
+    // static
+    public static string Email = "";
+    public static string Password = "";
+    
+
+    //public
+    public string currentMenu = "Login";
+
+    //private
+    private string CreateAccountUrl = "http://ramesh8856.pythonanywhere.com/signup";
+    private string LoginUrl = "http://ramesh8856.pythonanywhere.com/login";
+    private GUIStyle Heading;
+    private GUIStyle Label;
+
+    public string NEmail = "";
+    public string NPassword = "";
+    public string NCPassword = "";
+    public string HandPreference = "";
+    public string Age = "";
+
+    //GUI test
+    public float X;
+    public float Y;
+    public float Width;
+    public float Height;
+
+ #endregion
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+
+    void OnGUI() {
+        GUI.skin.textField.fontSize = 20;
+
+        GUI.skin.button.fontSize = 20;
+
+        Heading = new GUIStyle();
+        Heading.fontSize = 32;
+
+        Label = new GUIStyle();
+        Label.fontSize = 20;
+
+        //Label.border = ;
+        //Label.
+
+        if (currentMenu == "Login")
+        {
+            LoginGUI();
+        }else if(currentMenu == "CreateAccount")
+        {
+            CreateAccountGUI();
+        }
+    }
+
+    #region Custom methods
+    void LoginGUI() {
+        GUI.Box(new Rect(100, 20, Screen.width, Screen.height), "Login", Heading);
+        if(GUI.Button(new Rect(100, 290, 180, 40), "Create Account"))
+        {
+            currentMenu = "CreateAccount";
+        }
+
+        if(GUI.Button(new Rect(300, 290, 100, 40), "Login"))
+        {
+            StartCoroutine("LoginInit");
+        }
+
+        GUI.Label(new Rect(100, 100, 150, 30), "Email:", Label);
+        Email = GUI.TextField(new Rect(100, 130, (Screen.width / 2), 40), Email, 25);
+
+        GUI.Label(new Rect(100, 190, 150, 30), "Password:", Label);
+        Password = GUI.TextField(new Rect(100, 220, (Screen.width / 2) , 40), Password);
+
+    }
+
+    public void SceneSwitcher()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void CreateAccountGUI()
+    {
+        GUI.Box(new Rect(100, 20, Screen.width, Screen.height), "Create Account", Heading);
+        
+
+        GUI.Label(new Rect(100, 80, 150, 30), "Email:", Label);
+        NEmail = GUI.TextField(new Rect(100, 110, (Screen.width / 2), 40), NEmail);
+
+        GUI.Label(new Rect(100, 160, 150, 30), "Password:", Label);
+        NPassword = GUI.TextField(new Rect(100, 190, (Screen.width / 2), 40), NPassword);
+
+        GUI.Label(new Rect(100, 240, 150, 30), "Confirm Password:", Label);
+        NCPassword = GUI.TextField(new Rect(100, 270, (Screen.width / 2), 40), NCPassword);
+
+        GUI.Label(new Rect(100, 320, 140, 30), "Hand Preference", Label);
+        HandPreference = GUI.TextField(new Rect(100, 350, (Screen.width / 2), 40), HandPreference);
+
+        GUI.Label(new Rect(100, 400, 150, 30), "Age", Label);
+        Age = GUI.TextField(new Rect(100, 430, (Screen.width / 2), 40), Age);
+
+        if (GUI.Button(new Rect(100, 500, 180, 40), "Create Account")) { 
+        
+            //currentMenu = "CreateAccount";
+            if (NPassword == NCPassword)
+            {
+                StartCoroutine("CreateAccount");
+            }
+            else
+            {
+                //StartCoroutine();
+            }
+        }
+
+        if (GUI.Button(new Rect(420, 500, 100, 40), "Back"))
+        {
+            currentMenu = "Login";
+        }
+
+        
+    }
+
+    #endregion
+#region Couroutiune
+    IEnumerator CreateAccount() {
+        WWWForm Form = new WWWForm();
+        Form.AddField("Email", NEmail);
+        Form.AddField("Password", NPassword);
+        Form.AddField("Age", Age);
+        Form.AddField("HandPreference", HandPreference);
+        WWW CreateAccountWww = new WWW(CreateAccountUrl, Form);
+        yield return CreateAccountWww;
+
+        if(CreateAccountWww.error != null)
+        {
+            Debug.LogError("Cannot connect to Account creation");
+        }
+        else
+        {
+            string CreateAccountReturn = CreateAccountWww.text;
+            if (CreateAccountReturn == "Success")
+            {   
+                Debug.Log("Success: Account creation");
+                currentMenu = "Login";
+            }
+        }
+    }
+
+    IEnumerator LoginInit()
+    {
+        Debug.Log("Init: Login");
+        WWWForm Form = new WWWForm();
+        Form.AddField("Email", Email);
+        Form.AddField("Password", Password);
+        WWW LoginWww = new WWW(LoginUrl, Form);
+        yield return LoginWww;
+
+        if (LoginWww.error != null)
+        {
+            Debug.LogError("Cannot connect to Login");
+        }
+        else
+        {
+            Debug.Log(LoginWww.text);
+            string CreateAccountReturn = LoginWww.text;
+            if (CreateAccountReturn == "success")
+            {
+                Debug.Log("Success: Login");
+                currentMenu = "Login";
+            }
+        }
+        Debug.Log("Before Sceneswitcher function");
+        SceneSwitcher();
+
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/WelcomeScreenScripts/buttonHandler.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+
+public class buttonHandler : MonoBehaviour {
+
+    private Button myButton_instructions;
+    private Button myButton_play;
+    private Button backButton;
+    private Text instructions;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Button_OnClick()
+    {
+        Debug.Log("Instructions Button Clicked");
+
+        myButton_instructions = GameObject.Find("Button").GetComponent<Button>();
+        myButton_instructions.gameObject.SetActive(false);
+
+        
+        myButton_play = GameObject.Find("Button_Start Learning").GetComponent<Button>();
+        myButton_play.gameObject.SetActive(false);
+
+        backButton = getInactiveGameObject("Button_Back").GetComponent<Button>();
+        backButton.gameObject.SetActive(true);
+
+        instructions = getInactiveGameObject("Instructions").GetComponent<Text>();
+        instructions.gameObject.SetActive(true);
+    }
+
+    public void backButton_OnClick()
+    {
+        Debug.Log("Back Button Clicked");
+       
+        backButton = getInactiveGameObject("Button_Back").GetComponent<Button>();
+        backButton.gameObject.SetActive(false);
+
+        instructions = getInactiveGameObject("Instructions").GetComponent<Text>();
+        instructions.gameObject.SetActive(false);
+
+        myButton_instructions = getInactiveGameObject("Button").GetComponent<Button>();
+        myButton_instructions.gameObject.SetActive(true);
+
+        myButton_play = getInactiveGameObject("Button_Start Learning").GetComponent<Button>();
+        myButton_play.gameObject.SetActive(true);
+
+    }
+
+    public GameObject getInactiveGameObject(string objectName)
+    {
+        GameObject filedname = null;
+        Transform[] trans = GameObject.Find("Canvas").GetComponentsInChildren<Transform>(true);
+        foreach (Transform t in trans)
+        {
+            if (t.gameObject.name == objectName)
+            {
+                filedname = t.gameObject;
+            }
+        }
+
+        return filedname;
+    }
+
+    public void onClickStartLearning()
+    {
+        SceneManager.LoadScene("TutorialHome");
+    }
+}
