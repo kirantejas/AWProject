c99660a0cfae1debcb417e6c3c63e0f9873fed76
Kiran Teja Settipalli <kiranteja.settipalli@gmail.com>
2018-11-25 22:14:27 -0700

    Log time for exercise and quiz

--- Assets/Scripts/Exercise2.cs
+++ Assets/Scripts/Exercise2.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class Exercise2 : MonoBehaviour {
 
+    private readonly string url = "http://ramesh8856.pythonanywhere.com/";
     public InputField input;
     private string enteredtext;
     GameObject window;
     float timeLeft = 30.0f;
 
+    DateTime startTime;
+    DateTime endTime;
+    TimeSpan timeDiff;
+
     // Checks if there is anything entered into the input field.
 
     void Start ()
     {
+        startTime = DateTime.Now;
         Debug.Log("Hello");
     }
 
@@ -82,4 +89,44 @@ public class Exercise2 : MonoBehaviour {
     {
         SceneManager.LoadScene("Level3");
     }
+    public void onClickHome()
+    {
+        timeDiff = DateTime.Now - startTime;
+        StartCoroutine(LogLevelStats(Assets.Scripts.globalClass.Id, 6, (int)timeDiff.TotalSeconds));
+        SceneManager.LoadScene("WelcomeScreen");
+    }
+
+
+    public void onClickNext()
+    {
+        timeDiff = DateTime.Now - startTime;
+        StartCoroutine(LogLevelStats(Assets.Scripts.globalClass.Id, 6, (int)timeDiff.TotalSeconds));
+        SceneManager.LoadScene("WelcomeScreen");
+    }
+    #region Couroutiune
+
+    IEnumerator LogLevelStats(int userId, int level, int time)
+    {
+        WWWForm Form = new WWWForm();
+        Form.AddField("user_id", userId);
+        Form.AddField("level", level);
+        Form.AddField("time", time);
+        WWW CreateAccountWww = new WWW(url + "levelstats", Form);
+        yield return CreateAccountWww;
+
+        if (CreateAccountWww.error != null)
+        {
+            Debug.LogError("Cannot connect to Level stats creation");
+        }
+        else
+        {
+            string CreateAccountReturn = CreateAccountWww.text;
+            if (CreateAccountReturn == "success")
+            {
+                Debug.Log("Success: Level stats log entry");
+
+            }
+        }
+    }
+    #endregion
 }
